69ebdf9c366796e59a3564f4e6488e613971cc50
Dániel Buga <bugadani@gmail.com>
2021-04-04 12:23:22 +0200

    Add scroll widget

---
+++ examples/scrolling.rs
@@ -0,0 +1,139 @@
+use std::{thread, time::Duration};
+
+use backend_embedded_graphics::{
+    themes::default::primary_button, widgets::label::ascii::LabelConstructor, EgCanvas,
+};
+use embedded_graphics::{
+    draw_target::DrawTarget, pixelcolor::BinaryColor, prelude::Size as EgSize,
+};
+use embedded_graphics_simulator::{
+    sdl2::MouseButton, BinaryColorTheme, OutputSettingsBuilder, SimulatorDisplay, SimulatorEvent,
+    Window as SimWindow,
+};
+use embedded_gui::{
+    data::BoundData,
+    input::event::{InputEvent, PointerEvent, ScrollEvent},
+    widgets::{
+        label::Label,
+        layouts::linear::{column::Column, Cell},
+        primitives::border::Border,
+        scroll::Scroll,
+    },
+    Position, Window,
+};
+
+fn convert_input(event: SimulatorEvent) -> Result<InputEvent, bool> {
+    unsafe {
+        // This is fine for a demo
+        static mut MOUSE_DOWN: bool = false;
+        match event {
+            SimulatorEvent::MouseButtonUp {
+                mouse_btn: MouseButton::Left,
+                point,
+            } => {
+                MOUSE_DOWN = false;
+                Ok(InputEvent::PointerEvent(
+                    Position {
+                        x: point.x,
+                        y: point.y,
+                    },
+                    PointerEvent::Up,
+                ))
+            }
+            SimulatorEvent::MouseButtonDown {
+                mouse_btn: MouseButton::Left,
+                point,
+            } => {
+                MOUSE_DOWN = true;
+                Ok(InputEvent::PointerEvent(
+                    Position {
+                        x: point.x,
+                        y: point.y,
+                    },
+                    PointerEvent::Down,
+                ))
+            }
+            SimulatorEvent::MouseMove { point } => Ok(InputEvent::PointerEvent(
+                Position {
+                    x: point.x,
+                    y: point.y,
+                },
+                if MOUSE_DOWN {
+                    PointerEvent::Drag
+                } else {
+                    PointerEvent::Hover
+                },
+            )),
+
+            SimulatorEvent::MouseWheel { scroll_delta, .. } => {
+                // TODO: We could break this down into multiple scroll events
+                Ok(InputEvent::ScrollEvent(if scroll_delta.y != 0 {
+                    ScrollEvent::VerticalScroll(scroll_delta.y * 4)
+                } else {
+                    ScrollEvent::HorizontalScroll(scroll_delta.x * 4)
+                }))
+            }
+
+            SimulatorEvent::Quit => Err(true),
+            _ => Err(false),
+        }
+    }
+}
+
+fn main() {
+    let display = SimulatorDisplay::new(EgSize::new(32, 32));
+
+    let scroll_data = BoundData::new(0, |_| ());
+    // TODO: this example should also demonstrate a scrollbar and horizontal scroll widget
+    let mut gui = Window::new(
+        EgCanvas::new(display),
+        Border::new(
+            Scroll::vertical(
+                Column::new(Cell::new(Label::new("S")))
+                    .add(Cell::new(Label::new("c")))
+                    .add(Cell::new(Label::new("r")))
+                    .add(Cell::new(Label::new("o")))
+                    .add(Cell::new(primary_button("l")))
+                    .add(Cell::new(Label::new("l")))
+                    .add(Cell::new(Label::new("o")))
+                    .add(Cell::new(Label::new("l")))
+                    .add(Cell::new(Label::new("o"))),
+            )
+            .bind(scroll_data) // FIXME (maybe) - needs to be bound otherwise callback doesn't fire
+            .on_scroll_changed(|_, _pd| {
+                //println!("Scroll offset: {:?}", pd);
+            }),
+        ),
+    );
+
+    let output_settings = OutputSettingsBuilder::new()
+        .theme(BinaryColorTheme::OledBlue)
+        .build();
+    let mut window = SimWindow::new("GUI demonstration", &output_settings);
+
+    loop {
+        gui.canvas.target.clear(BinaryColor::Off).unwrap();
+
+        gui.update();
+        gui.measure();
+        gui.arrange();
+        gui.draw().unwrap();
+
+        // Update the window.
+        window.update(&gui.canvas.target);
+
+        // Handle key and mouse events.
+        for event in window.events() {
+            match convert_input(event) {
+                Ok(input) => {
+                    gui.input_event(input);
+                }
+                Err(true) => return,
+                _ => {}
+            }
+        }
+
+        // Wait for a little while.
+        thread::sleep(Duration::from_millis(10));
+    }
+}
---
+++ src/widgets/scroll.rs
@@ -0,0 +1,467 @@
+use crate::{
+    data::{NoData, WidgetData},
+    input::{
+        controller::InputContext,
+        event::{InputEvent, PointerEvent, ScrollEvent},
+    },
+    widgets::{ParentHolder, UpdateHandler, Widget, WidgetDataHolder, WidgetStateHolder},
+    BoundingBox, Canvas, MeasureConstraint, MeasureSpec, MeasuredSize, Position, PositionDelta,
+    WidgetRenderer, WidgetState,
+};
+
+pub struct ScrollData {
+    /// The current scroll position.
+    pub offset: u32,
+
+    /// The largest possible value of `offset`.
+    pub maximum_offset: u32,
+
+    /// The Scroll widget's main size (height of Vertical, width of Horizontal).
+    pub viewport_size: u32,
+}
+
+pub trait ScrollDirection {
+    fn offset(&self) -> PositionDelta;
+    fn change_offset(&mut self, delta: PositionDelta);
+    fn override_offset(&mut self, offset: PositionDelta);
+    fn derive_measure_spec(spec: MeasureSpec) -> MeasureSpec;
+    fn scroll_direction<T>(x: T, y: T) -> T;
+    fn cross_direction<T>(x: T, y: T) -> T;
+}
+
+pub struct Horizontal {
+    offset: u32,
+}
+
+impl ScrollDirection for Horizontal {
+    fn offset(&self) -> PositionDelta {
+        PositionDelta {
+            x: self.offset as i32,
+            y: 0,
+        }
+    }
+
+    fn change_offset(&mut self, delta: PositionDelta) {
+        self.offset = (self.offset as i32 + delta.x).max(0) as u32;
+    }
+
+    fn override_offset(&mut self, offset: PositionDelta) {
+        self.offset = Self::scroll_direction(offset.x, offset.y) as u32;
+    }
+
+    fn derive_measure_spec(spec: MeasureSpec) -> MeasureSpec {
+        MeasureSpec {
+            width: MeasureConstraint::Unspecified,
+            height: spec.height,
+        }
+    }
+
+    fn scroll_direction<T>(x: T, _y: T) -> T {
+        x
+    }
+
+    fn cross_direction<T>(_x: T, y: T) -> T {
+        y
+    }
+}
+
+pub struct Vertical {
+    offset: u32,
+}
+
+impl ScrollDirection for Vertical {
+    fn offset(&self) -> PositionDelta {
+        PositionDelta {
+            x: 0,
+            y: self.offset as i32,
+        }
+    }
+
+    fn change_offset(&mut self, delta: PositionDelta) {
+        self.offset = (self.offset as i32 + delta.y).max(0) as u32;
+    }
+
+    fn override_offset(&mut self, offset: PositionDelta) {
+        self.offset = Self::scroll_direction(offset.x, offset.y) as u32;
+    }
+
+    fn derive_measure_spec(spec: MeasureSpec) -> MeasureSpec {
+        MeasureSpec {
+            width: spec.width,
+            height: MeasureConstraint::Unspecified,
+        }
+    }
+
+    fn scroll_direction<T>(_x: T, y: T) -> T {
+        y
+    }
+
+    fn cross_direction<T>(x: T, _y: T) -> T {
+        x
+    }
+}
+
+// Need separation because scroll change listeners need the fields.
+pub struct ScrollFields<W, SD, D> {
+    pub parent_index: usize,
+    pub inner: W,
+    pub bounds: BoundingBox,
+    pub on_state_changed: fn(&mut Self, WidgetState),
+    pub direction: SD,
+    pub state: WidgetState,
+    pub last_pointer_pos: Option<Position>,
+    pub on_scroll_changed: fn(&mut D, ScrollData),
+}
+
+impl Scroll<(), (), NoData> {
+    const STATE_IDLE: u32 = 0;
+    const STATE_HOVERED: u32 = 1;
+}
+
+pub struct Scroll<W, SD, D = NoData>
+where
+    D: WidgetData,
+{
+    pub fields: ScrollFields<W, SD, D::Data>,
+    data_holder: WidgetDataHolder<ScrollFields<W, SD, D::Data>, D>,
+}
+
+impl<W> Scroll<W, Horizontal, NoData>
+where
+    W: Widget,
+{
+    pub fn horizontal(inner: W) -> Self {
+        Scroll {
+            fields: ScrollFields {
+                parent_index: 0,
+                inner,
+                bounds: BoundingBox::default(),
+                on_state_changed: |_, _| (),
+                direction: Horizontal { offset: 0 },
+                state: WidgetState::default(),
+                last_pointer_pos: None,
+                on_scroll_changed: |_, _| (),
+            },
+            data_holder: WidgetDataHolder::default(),
+        }
+    }
+}
+
+impl<W> Scroll<W, Vertical, NoData>
+where
+    W: Widget,
+{
+    pub fn vertical(inner: W) -> Self {
+        Scroll {
+            fields: ScrollFields {
+                parent_index: 0,
+                inner,
+                bounds: BoundingBox::default(),
+                on_state_changed: |_, _| (),
+                direction: Vertical { offset: 0 },
+                state: WidgetState::default(),
+                last_pointer_pos: None,
+                on_scroll_changed: |_, _| (),
+            },
+            data_holder: WidgetDataHolder::default(),
+        }
+    }
+}
+
+impl<W, SD> Scroll<W, SD, NoData>
+where
+    W: Widget,
+{
+    pub fn bind<D>(self, data: D) -> Scroll<W, SD, D>
+    where
+        D: WidgetData,
+    {
+        Scroll {
+            fields: ScrollFields {
+                parent_index: self.fields.parent_index,
+                inner: self.fields.inner,
+                bounds: self.fields.bounds,
+                on_state_changed: |_, _| (),
+                direction: self.fields.direction,
+                state: self.fields.state,
+                last_pointer_pos: self.fields.last_pointer_pos,
+                on_scroll_changed: |_, _| (),
+            },
+            data_holder: WidgetDataHolder::new(data),
+        }
+    }
+}
+
+impl<W, SD, D> Scroll<W, SD, D>
+where
+    W: Widget,
+    D: WidgetData,
+    SD: ScrollDirection,
+{
+    pub fn on_data_changed(
+        mut self,
+        callback: fn(&mut ScrollFields<W, SD, D::Data>, &D::Data),
+    ) -> Self
+    where
+        D: WidgetData,
+    {
+        self.data_holder.on_data_changed = callback;
+        self
+    }
+
+    pub fn on_scroll_changed(mut self, callback: fn(&mut D::Data, ScrollData)) -> Self
+    where
+        D: WidgetData,
+    {
+        self.fields.on_scroll_changed = callback;
+        self
+    }
+
+    fn change_offset(&mut self, offset: PositionDelta) {
+        self.fields.direction.change_offset(offset);
+
+        // Clamp the offset.
+        let child_size = self.fields.inner.bounding_box().size;
+        let own_size = self.bounding_box().size;
+
+        let PositionDelta { x: dx, y: dy } = self.fields.direction.offset();
+
+        let max_offset_x = child_size.width.saturating_sub(own_size.width);
+        let max_offset_y = child_size.height.saturating_sub(own_size.height);
+
+        let offset = PositionDelta {
+            x: dx.min(max_offset_x as i32),
+            y: dy.min(max_offset_y as i32),
+        };
+
+        // Apply clamping.
+        self.fields.direction.override_offset(offset);
+
+        // Fire callback
+        let scroll_data = ScrollData {
+            maximum_offset: SD::scroll_direction(max_offset_x, max_offset_y),
+            offset: SD::scroll_direction(offset.x as u32, offset.y as u32),
+            viewport_size: SD::scroll_direction(own_size.width, own_size.height),
+        };
+
+        let callback = self.fields.on_scroll_changed;
+        self.data_holder
+            .data
+            .update(|data| callback(data, scroll_data));
+    }
+}
+
+impl<W, SD, D> WidgetStateHolder for Scroll<W, SD, D>
+where
+    W: Widget,
+    D: WidgetData,
+{
+    fn change_state(&mut self, state: u32) {
+        self.fields.state.change_state(state);
+    }
+
+    fn change_selection(&mut self, state: bool) {
+        self.fields.state.change_selection(state);
+    }
+
+    fn is_selectable(&self) -> bool {
+        false
+    }
+}
+
+impl<W, SD, D> Widget for Scroll<W, SD, D>
+where
+    W: Widget,
+    SD: ScrollDirection,
+    D: WidgetData,
+{
+    fn attach(&mut self, parent: usize, self_index: usize) {
+        self.set_parent(parent);
+        self.fields.inner.attach(self_index, self_index + 1);
+    }
+
+    fn arrange(&mut self, position: Position) {
+        let offset = self.fields.direction.offset();
+
+        self.fields.inner.arrange(position - offset);
+        self.bounding_box_mut().position = position;
+    }
+
+    fn bounding_box(&self) -> BoundingBox {
+        self.fields.bounds
+    }
+
+    fn bounding_box_mut(&mut self) -> &mut BoundingBox {
+        &mut self.fields.bounds
+    }
+
+    fn measure(&mut self, measure_spec: MeasureSpec) {
+        let child_spec = SD::derive_measure_spec(measure_spec);
+        self.fields.inner.measure(child_spec);
+
+        // Scroll is as big as parent lets it to be, children are (depending on the direction)
+        // as big as they want to be. If parent gives us an unspecified dimension, scroll will take
+        // up as much space as the child.
+        // TODO: maybe minor direction size should be equal to child's (e.g. Horizontal's height)
+        //       except when spec is Exactly
+        let child_size = self.fields.inner.bounding_box().size;
+        let size = MeasuredSize {
+            width: measure_spec.width.largest().unwrap_or(child_size.width),
+            height: measure_spec.height.largest().unwrap_or(child_size.height),
+        };
+        self.set_measured_size(size);
+    }
+
+    fn children(&self) -> usize {
+        1 + self.fields.inner.children()
+    }
+
+    fn get_child(&self, idx: usize) -> &dyn Widget {
+        if idx == 0 {
+            &self.fields.inner
+        } else {
+            self.fields.inner.get_child(idx - 1)
+        }
+    }
+
+    fn get_mut_child(&mut self, idx: usize) -> &mut dyn Widget {
+        if idx == 0 {
+            &mut self.fields.inner
+        } else {
+            self.fields.inner.get_mut_child(idx - 1)
+        }
+    }
+
+    fn test_input(&mut self, event: InputEvent) -> Option<usize> {
+        match event {
+            InputEvent::PointerEvent(position, PointerEvent::Hover) => {
+                // Need to keep track of hovered state because a Scroll event has no position
+                self.change_state(if self.bounding_box().contains(position) {
+                    Scroll::STATE_HOVERED
+                } else {
+                    Scroll::STATE_IDLE
+                });
+
+                self.fields.inner.test_input(event).map(|idx| idx + 1)
+            }
+
+            InputEvent::ScrollEvent(_) => self
+                .fields
+                .inner
+                .test_input(event)
+                .map(|idx| idx + 1)
+                .or_else(|| {
+                    if self.fields.state.state() == Scroll::STATE_HOVERED {
+                        Some(0)
+                    } else {
+                        None
+                    }
+                }),
+
+            InputEvent::PointerEvent(position, PointerEvent::Down) => {
+                // Pointer down = start drag-scrolling
+                if let Some(idx) = self.fields.inner.test_input(event) {
+                    // we give priority to our child
+                    Some(idx + 1)
+                } else if self.bounding_box().contains(position) {
+                    // Avoid jumping when some events were handled by children.
+                    self.fields.last_pointer_pos = None;
+                    Some(0)
+                } else {
+                    None
+                }
+            }
+
+            InputEvent::PointerEvent(_, PointerEvent::Drag) => {
+                // TODO: we should check if the drag originated inside the scroll widget - even if handled by child?
+                if self.fields.state.state() == Scroll::STATE_HOVERED {
+                    Some(0)
+                } else {
+                    None
+                }
+            }
+
+            // We only get Up if we handled Down
+            InputEvent::PointerEvent(_, PointerEvent::Up) => {
+                if self.fields.state.state() == Scroll::STATE_HOVERED {
+                    Some(0)
+                } else {
+                    None
+                }
+            }
+
+            _ => self.fields.inner.test_input(event).map(|idx| idx + 1),
+        }
+        // TODO: multiple interaction modes:
+        // touch should drag the inner widgets, mouse probably shouldn't
+    }
+
+    fn handle_input(&mut self, _ctxt: InputContext, event: InputEvent) -> bool {
+        let hovered = self.fields.state.state() == Scroll::STATE_HOVERED;
+        match event {
+            InputEvent::ScrollEvent(ScrollEvent::HorizontalScroll(dx)) => {
+                self.change_offset(PositionDelta { x: -dx, y: 0 });
+                true
+            }
+
+            InputEvent::ScrollEvent(ScrollEvent::VerticalScroll(dy)) => {
+                self.change_offset(PositionDelta { x: 0, y: -dy });
+                true
+            }
+
+            InputEvent::PointerEvent(position, PointerEvent::Drag) if hovered => {
+                if let Some(last) = self.fields.last_pointer_pos {
+                    self.change_offset(last - position);
+                }
+                self.fields.last_pointer_pos = Some(position);
+                true
+            }
+
+            InputEvent::PointerEvent(position, PointerEvent::Up) if hovered => {
+                if let Some(last) = self.fields.last_pointer_pos {
+                    self.change_offset(last - position);
+                }
+                self.fields.last_pointer_pos = None;
+                true
+            }
+
+            _ => false,
+        }
+    }
+}
+
+impl<W, SD, D> UpdateHandler for Scroll<W, SD, D>
+where
+    W: Widget,
+    SD: ScrollDirection,
+    D: WidgetData,
+{
+    fn update(&mut self) {
+        self.data_holder.update(&mut self.fields);
+    }
+}
+
+impl<W, SD, D> ParentHolder for Scroll<W, SD, D>
+where
+    W: Widget,
+    D: WidgetData,
+{
+    fn parent_index(&self) -> usize {
+        self.fields.parent_index
+    }
+
+    fn set_parent(&mut self, index: usize) {
+        self.fields.parent_index = index;
+    }
+}
+
+impl<C, W, SD, D> WidgetRenderer<C> for Scroll<W, SD, D>
+where
+    W: Widget + WidgetRenderer<C>,
+    C: Canvas,
+    D: WidgetData,
+{
+    fn draw(&self, canvas: &mut C) -> Result<(), C::Error> {
+        self.fields.inner.draw(canvas)
+    }
+}
