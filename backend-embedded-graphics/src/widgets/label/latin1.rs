c6f0a7bf72f7a928991848246eaccb23c20f59bc
Dániel Buga <bugadani@gmail.com>
2021-04-21 18:42:34 +0200

    Simplify ascii label constructor

--- backend-embedded-graphics/src/widgets/label/latin1.rs
+++ backend-embedded-graphics/src/widgets/label/latin1.rs
@@ -1,5 +1,4 @@
 use embedded_graphics::{
-    draw_target::DrawTarget,
     mono_font::{latin1, MonoTextStyle, MonoTextStyleBuilder},
     pixelcolor::PixelColor,
 };
@@ -7,20 +6,17 @@ use embedded_gui::{geometry::BoundingBox, widgets::label::Label};
 
 use crate::{themes::Theme, widgets::label::LabelStyle};
 
-pub trait LabelConstructor<S, C, D> {
-    fn new(text: S) -> Label<S, LabelStyle<MonoTextStyle<D::Color, latin1::Font6x10>>>
+pub trait LabelConstructor<S, C> {
+    fn new(text: S) -> Label<S, LabelStyle<MonoTextStyle<C, latin1::Font6x10>>>
     where
         C: PixelColor,
-        D: DrawTarget<Color = C>,
         S: AsRef<str>;
 }
 
-impl<C, D, S> LabelConstructor<S, C, D>
-    for Label<S, LabelStyle<MonoTextStyle<D::Color, latin1::Font6x10>>>
+impl<C, S> LabelConstructor<S, C> for Label<S, LabelStyle<MonoTextStyle<C, latin1::Font6x10>>>
 where
     S: AsRef<str>,
     C: PixelColor + Theme,
-    D: DrawTarget<Color = C>,
 {
     fn new(text: S) -> Self {
         Label {
@@ -29,7 +25,7 @@ where
             label_properties: LabelStyle {
                 renderer: MonoTextStyleBuilder::new()
                     .font(latin1::Font6x10)
-                    .text_color(<D::Color as Theme>::TEXT_COLOR)
+                    .text_color(<C as Theme>::TEXT_COLOR)
                     .build(),
             },
             bounds: BoundingBox::default(),
