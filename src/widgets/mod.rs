ccd0841eee631989669e67ef3d18a504706d6960
Dániel Buga <bugadani@gmail.com>
2021-08-14 13:32:48 +0200

    Add _ example

--- src/widgets/mod.rs
+++ src/widgets/mod.rs
@@ -1,6 +1,65 @@
 //! Base widgets
+//! ============
 //!
 //! *Note*: this list is not exhaustive as backend crates may also define custom widgets.
+//!
+//! Combining widgets
+//! -----------------
+//!
+//! The set of widgets have been designed to do as little as necessary. This means that you have
+//! different widgets to specify background color, border color and thickness, and so on.
+//!
+//! You are free to combine these widgets. As an example, you can layer two `Border` widgets and
+//! a `Spacing` to create a double border effect.
+//!
+//! Backend crates might define certain arrangements to simplify usage.
+//!
+//! Helper functions
+//! ----------------
+//!
+//! Sometimes we want to reuse a particular combination of widgets. To do this, we can create
+//! functions that returns the nested structure we describe.
+//!
+//! Due to how `embedded-gui` is built, we must specify the exact type of the nested widget
+//! structure. Thankfully, if we did our part well, the compiler can help us with this.
+//!
+//! Consider the following example, where we want to create function that constructs a
+//! `TextBlock` with a border. We can implement our function like this, purposefully omitting the
+//! return type:
+//!
+//! ```rust,ignore
+//! fn button_with_style<W: Widget>(inner: W) -> _ {
+//!     Button::new(
+//!         // we need to help the compiler a bit
+//!         Background::<_, BackgroundStyle<BinaryColor>>::new(
+//!             Border::<_, BorderStyle<BinaryColor>>::new(
+//!                 FillParent::both(inner)
+//!                     .align_horizontal(Center)
+//!                     .align_vertical(Center),
+//!             )
+//!             .border_color(BinaryColor::Off),
+//!         )
+//!         .background_color(BinaryColor::Off),
+//!     )
+//! }
+//! ```
+//!
+//! If we try to build this, the compiler will give us the correct return type:
+//!
+//! ```text
+//! error[E0121]: the type placeholder `_` is not allowed within types on item signatures for return types
+//!    --> examples\hello_world.rs:108:46
+//!     |
+//! 108 | fn button_with_style<W: Widget>(inner: W) -> _ {
+//!     |                                              ^
+//!     |                                              |
+//!     |                                              not allowed in type signatures
+//!     |                                              help: replace with the correct return type: `Button<Background<Border<FillParent<W, HorizontalAndVertical, embedded_gui::widgets::fill::Center, embedded_gui::widgets::fill::Center>, BorderStyle<BinaryColor>>, BackgroundStyle<BinaryColor>>>`
+//! ```
+//!
+//! If we have supplied the compiler with enough information, it will be able to tell us the exact
+//! return type we need to paste in place of the `_`.
+//!
 
 use crate::{
     data::WidgetData,
